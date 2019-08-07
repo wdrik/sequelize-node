09eaeea2ed721a49836d7a2a9ed9c8b27c333a04
Iorgen Wildrik Souza da Silva <iosilva@i4pro.com.br>
2019-08-07 16:25:55 -0300

    Sequelize config and first migration

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-    res.json({ hello: "World" });
+app.get('/', (req, res) => {
+  res.json({ hello: 'World' });
 });
 
 app.listen(3000);
