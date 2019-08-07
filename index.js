2a53e3e5ac5fa664b176363da86088541be4d455
Iorgen Wildrik Souza da Silva <iosilva@i4pro.com.br>
2019-08-07 16:47:29 -0300

    Methods REST

--- index.js
+++ index.js
@@ -12,4 +12,18 @@ app.listen(3000);
 
 const { User } = require('./app/models');
 
-User.create({ name: 'Iorgen', email: 'iorgen@rocketseat.com', password: '123456' });
+app.get('/users', (req, res) => {}); //Listar todos
+app.post('/users', (req, res) => {}); // Criar
+app.get('/users/:id', (req, res) => {}); //Buscar
+app.put('/users/:id', (req, res) => {}); //Editar
+app.delete('/users/:id', (req, res) => {}); //Deletar
+
+app.post('/register', async (req, res) => {
+  console.log(req.body);
+  return;
+  const user = await User.create(req.body);
+
+  res.json(user);
+});
+
+// User.create({ name: 'Iorgen', email: 'iorgen@rocketseat.com', password: '123456' });
